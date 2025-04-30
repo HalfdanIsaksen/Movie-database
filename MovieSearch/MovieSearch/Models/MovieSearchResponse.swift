728ea8b0382852521ae8e7d65c22ab5462688df5
Halfdan Albrecht Isaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2025-04-30 15:24:55 +0100

    fetching movie data

--- MovieSearch/MovieSearch/Models/MovieSearchResponse.swift
+++ MovieSearch/MovieSearch/Models/MovieSearchResponse.swift
@@ -4,4 +4,32 @@
 //
 //  Created by Halfdan Albrecht Isaksen on 30/04/2025.
 //
+import Foundation
 
+func searchMovies(query: String, language: String = "en-US") async{
+    do{
+        let url = URL(string: "https://api.themoviedb.org/3/search/movie")!
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+        let queryItems: [URLQueryItem] = [
+            URLQueryItem(name: "query", value: query),
+            URLQueryItem(name: "include_adult", value: "true"),
+            URLQueryItem(name: "language", value: language),
+            URLQueryItem(name: "page", value: "1"),
+        ]
+        components.queryItems = components.queryItems.map { $0 + queryItems } ?? queryItems
+        
+        var request = URLRequest(url: components.url!)
+        request.httpMethod = "GET"
+        request.timeoutInterval = 10
+        request.allHTTPHeaderFields = [
+            "accept": "application/json",
+            "Authorization": "xx"
+        ]
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        print(String(decoding: data, as: UTF8.self))
+    }catch{
+        print("Error fetching movie data: \(error)")
+    }
+    
+}

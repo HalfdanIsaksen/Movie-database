b0e0ea4b536a1a0c4c89c3f2277ca8eb4c58ef2a
Halfdan Albrecht Isaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2025-05-03 22:21:59 +0100

    search

---
+++ MovieSearch/MovieSearch/Views/SearchField.swift
@@ -0,0 +1,14 @@
+//
+//  SearchField.swift
+//  MovieSearch
+//
+//  Created by Halfdan Albrecht Isaksen on 03/05/2025.
+//
+
+import SwiftUI
+
+struct SearchField: View {
+    var body: some View {
+        TextField("Search", text: .constant("Search..."))
+    }
+}

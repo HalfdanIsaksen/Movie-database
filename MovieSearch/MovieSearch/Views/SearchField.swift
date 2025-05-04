326cd6c70ab5ffd55cdfffc3e4b0321ccb96dc22
Halfdan Albrecht Isaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2025-05-04 22:54:25 +0100

    still doesnt work

--- MovieSearch/MovieSearch/Views/SearchField.swift
+++ MovieSearch/MovieSearch/Views/SearchField.swift
@@ -4,13 +4,18 @@
 //
 //  Created by Halfdan Albrecht Isaksen on 03/05/2025.
 //
-
+import Foundation
 import SwiftUI
 
 struct SearchField: View {
     @State var searchText: String = ""
     var body: some View {
         TextField("Search...", text: $searchText)
+            .onChange(of: searchText) { newValue in
+                            Task {
+                                await searchMovies(query: newValue)
+                            }
+                        }
     }
 }
 #Preview {

6fe1fd7ae9b8bad1dc12c2b61af3ce3055fd7974
Halfdan Albrecht Isaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2025-05-16 12:02:57 +0200

    change for more modular approache

---
+++ MovieSearch/MovieSearch/Views/UserInfoView.swift
@@ -0,0 +1,51 @@
+//
+//  User.swift
+//  MovieSearch
+//
+//  Created by Halfdan Albrecht Isaksen on 15/05/2025.
+//
+
+import SwiftUI
+
+struct UserInfoView: View {
+        @Binding var user: UserModel
+        @State private var isImagePickerPresented = false
+        @State private var inputImage: UIImage?
+
+        var profileImage: Image {
+        if let data = user.profileImageData, let uiImage = UIImage(data: data) {
+            return Image(uiImage: uiImage)
+            } else {
+                return Image(systemName: "person.circle")
+            }
+        }
+
+        var body: some View {
+           HStack {
+               profileImage
+                   .resizable()
+                   .scaledToFit()
+                   .frame(width: 100, height: 100)
+                   .clipShape(Circle())
+                   .onTapGesture {
+                       isImagePickerPresented = true
+                   }
+
+               VStack(alignment: .leading) {
+                   TextField("Enter your name", text: $user.name)
+                       .font(.headline)
+
+                   DatePicker("Birthday", selection: $user.birthday, displayedComponents: .date)
+                       .font(.subheadline)
+               }
+           }
+           .sheet(isPresented: $isImagePickerPresented, onDismiss: loadImage) {
+               ImagePicker(image: $inputImage)
+           }
+        }
+
+        private func loadImage() {
+           guard let inputImage = inputImage else { return }
+           user.profileImageData = inputImage.jpegData(compressionQuality: 0.8)
+        }
+}

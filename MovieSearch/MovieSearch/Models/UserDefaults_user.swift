99b61aad9d0f17221de4f4aa428d17e1bf05033e
Halfdan Albrecht Isaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2025-05-21 10:48:30 +0200

    user defaults save and load trying to get added

---
+++ MovieSearch/MovieSearch/Models/UserDefaults_user.swift
@@ -0,0 +1,30 @@
+//
+//  UserDefaults_user.swift
+//  MovieSearch
+//
+//  Created by Halfdan Albrecht Isaksen on 21/05/2025.
+//
+
+import Foundation
+
+extension UserDefaults {
+    private static let userKey = "currentUser"
+
+    func saveUser(_ user: User) {
+        if let data = try? JSONEncoder().encode(user) {
+            set(data, forKey: Self.userKey)
+        }
+    }
+
+    func loadUser() -> User? {
+        guard let data = data(forKey: Self.userKey),
+              let user = try? JSONDecoder().decode(User.self, from: data) else {
+            return nil
+        }
+        return user
+    }
+
+    func removeUser() {
+        removeObject(forKey: Self.userKey)
+    }
+}

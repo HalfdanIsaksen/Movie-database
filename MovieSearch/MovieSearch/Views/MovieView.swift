3406539fa14159166702b53a62aecb6746d20e6e
Halfdan Albrecht Isaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2025-06-12 13:26:16 +0200

    movie view to be displayed if a movie if clicked on

---
+++ MovieSearch/MovieSearch/Views/MovieView.swift
@@ -0,0 +1,17 @@
+//
+//  MovieView.swift
+//  MovieSearch
+//
+//  Created by Halfdan Albrecht Isaksen on 12/06/2025.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+public struct MovieView: View {
+    
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}

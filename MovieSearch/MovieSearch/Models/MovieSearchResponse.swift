3978c4d9ca2380010e99ff12e2ee909c2279d4a8
Halfdan Albrecht Isaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2025-06-23 19:22:12 +0200

    Popular movies query done

--- MovieSearch/MovieSearch/Models/MovieSearchResponse.swift
+++ MovieSearch/MovieSearch/Models/MovieSearchResponse.swift
@@ -130,8 +130,14 @@ class MovieSearchViewModel: ObservableObject {
             return [] // <-- ensure function always returns a [Movie]
         }
     }
+    
     func popularMovies() async throws -> [Movie] {
         do{
+            guard let apiKey = Bundle.main.infoDictionary?["TMDB_API_KEY"] as? String else {
+                print("API Key missing.")
+                return [] // <-- return an empty array instead of nothing
+            }
+            
             let url = URL(string: "https://api.themoviedb.org/3/movie/popular")!
             var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
             let queryItems: [URLQueryItem] = [
@@ -142,13 +148,17 @@ class MovieSearchViewModel: ObservableObject {
             
             var request = URLRequest(url: components.url!)
             request.httpMethod = "GET"
-            request.timeoutInterval = 10
-            request.allHTTPHeaderFields = [
-                "accept": "application/json",
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YWQ3YmNlYTAwMTIzZWQ2YTMwZTc4MjE4ODBlMzNkMSIsIm5iZiI6MTc0NjAyMDYxNS43NTksInN1YiI6IjY4MTIyOTA3YzZjMDIxZWVmNzE0NTczOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VGBXTpvLzXfND_PpQR86t-WQZy2yFutPjp2sqqktZYU"
-            ]
+            request.timeoutInterval = 90
+            request.setValue("application/json", forHTTPHeaderField: "accept")
+            request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
             
             let (data, _) = try await URLSession.shared.data(for: request)
+            print(String(decoding: data, as: UTF8.self))
+            
+            // Decode JSON into [Movie]
+            let decoder = JSONDecoder()
+            let response = try decoder.decode(MovieSearchResponse.self, from: data)
+            return response.results
         }catch{
             if (error as? URLError)?.code == .cancelled {
                 print("Search cancelled for favorited movies")

0ba4027decf0329698b73b8c495f49310e6bbde6
Halfdan Albrecht Isaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2025-06-24 23:29:15 +0200

    popular integration

--- MovieSearch/MovieSearch/Views/ExploreView.swift
+++ MovieSearch/MovieSearch/Views/ExploreView.swift
@@ -13,16 +13,19 @@ struct ExploreView: View {
     @StateObject private var movieSearchModel = MovieSearchViewModel()
 
     @State private var trendingMovies: [Movie] = []
+    @State private var popularMovies: [Movie] = []
 
     var body: some View {
         VStack(alignment: .leading){
             //MovieColumn(title: "Recommended for You", movies: recommendedMovies, userViewModel: userViewModel)
             MovieColumn(title: "Trending", movies: trendingMovies, userViewModel: userViewModel)
+            MovieColumn(title: "Popular", movies: <#T##[Movie]#>, userViewModel: <#T##UserViewModel#>)
         }
         .navigationTitle("Home")
         .task {
             // Run when the view appears
             trendingMovies = (try? await movieSearchModel.trendingMovies()) ?? []
+            
         }
     }
 }

78cd24cd4ec9f8063437125a375f7a6ca39d3c1e
Halfdan Albrecht Isaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2025-09-05 11:45:43 +0200

    upcoming movies added

--- MovieSearch/MovieSearch/Models/MovieSearchResponse.swift
+++ MovieSearch/MovieSearch/Models/MovieSearchResponse.swift
@@ -236,5 +236,4 @@ class MovieSearchViewModel: ObservableObject {
             return []
         }
     }
-
 }
--- MovieSearch/MovieSearch/Views/ExploreView.swift
+++ MovieSearch/MovieSearch/Views/ExploreView.swift
@@ -15,17 +15,23 @@ struct ExploreView: View {
     @State private var trendingMovies: [Movie] = []
     @State private var popularMovies: [Movie] = []
     @State private var topRatedMovies: [Movie] = []
+    @State private var upcomingMovies: [Movie] = []
 
     var body: some View {
-        VStack(alignment: .leading){
-            //MovieColumn(title: "Recommended for You", movies: recommendedMovies, userViewModel: userViewModel)
-            MovieColumn(title: "Trending", movies: trendingMovies, userViewModel: userViewModel)
-            MovieColumn(title: "Popular", movies: popularMovies, userViewModel: userViewModel)
-            MovieColumn(title: "Top Rated", movies: topRatedMovies, userViewModel: userViewModel)
-        }
+        ScrollView(.vertical, showsIndicators: true) {
+                VStack(alignment: .leading, spacing: 20) {
+                    //MovieColumn(title: "Recommended for You", movies: recommendedMovies, userViewModel: userViewModel)
+                    MovieColumn(title: "Upcoming Movies", movies: upcomingMovies, userViewModel: userViewModel)
+                    MovieColumn(title: "Trending", movies: trendingMovies, userViewModel: userViewModel)
+                    MovieColumn(title: "Popular", movies: popularMovies, userViewModel: userViewModel)
+                    MovieColumn(title: "Top Rated", movies: topRatedMovies, userViewModel: userViewModel)
+                }
+                .padding(.vertical)
+            }
         .navigationTitle("Home")
         .task {
             // Run when the view appears
+            upcomingMovies = (try? await movieSearchModel.upcomingMovies()) ?? []
             trendingMovies = (try? await movieSearchModel.trendingMovies()) ?? []
             popularMovies  = (try? await movieSearchModel.popularMovies()) ?? []
             topRatedMovies = (try? await movieSearchModel.topratedMovies()) ?? []

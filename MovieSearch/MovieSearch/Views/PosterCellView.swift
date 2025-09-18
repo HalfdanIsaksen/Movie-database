07f8f021159c07cc8aaee3b9a8ce16da7b1336df
Halfdan Albrecht Isaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2025-09-18 14:33:31 +0200

    Postercell
    Should probably have moviecardview match postercell and just have on to display movies

---
+++ MovieSearch/MovieSearch/Views/PosterCellView.swift
@@ -0,0 +1,31 @@
+//
+//  PosterCellView.swift
+//  MovieSearch
+//
+//  Created by Halfdan Albrecht Isaksen on 18/09/2025.
+//
+import SwiftUI
+
+struct PosterCell: View {
+    let movie: MovieModel
+    let userViewModel: UserViewModel
+    var body: some View {
+        ZStack(alignment: .bottomLeading) {
+            // Poster image fills the cell
+            AsyncImage(url: movie.posterURL) { img in
+                img.resizable().scaledToFill()
+            } placeholder: {
+                Rectangle().fill(.secondary.opacity(0.2))
+            }
+
+            // Title overlay (optional)
+            LinearGradient(colors: [.black.opacity(0.0), .black.opacity(0.6)], startPoint: .center, endPoint: .bottom)
+            Text(movie.title)
+                .font(.subheadline.weight(.semibold))
+                .foregroundColor(.white)
+                .padding(8)
+        }
+        .contentShape(Rectangle())
+        .onTapGesture { /* open details */ }
+    }
+}
